b1920edf22e3689bfb2287aaa1c2cab3b97399b6
anitha1405 <anitha14051995@gmail.com>
2016-08-13 13:04:21 +0530

    Create v.c

---
+++ v.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char s;
+scanf("%c",&s);
+if(s=='A'&&s=='E'&&s=='I'&&s=='O'&&s=='U'&&s=='e'&&s=='a'&&s='i'&&s=='o'&&s=='u')
+printf("vowel\n")
+else
+printf("consonant");
+return 0;
+}
